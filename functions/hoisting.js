562c5e1ed1abb0d260a3e9b9f17ece37943e0c7e
Raghav000001 <braghav474@gmail.com>
2026-01-08 13:48:37 +0530

    functions basics

---
+++ functions/hoisting.js
@@ -0,0 +1,15 @@
+ 
+    sayHello()
+    function sayHello() {
+        console.log("hello");
+    }
+
+
+//    not allowed
+
+    // message()
+   
+    // const message = function(){
+    //     console.log("hello from variable function");
+        
+    // }
